54f6f2a9f2573beb569c816eb7f1f60f9ec82d6b
plabon <shaeedplabon@gmail.com>
2025-06-30 12:21:03 +0600

    update

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 3000
 const app = express()
-app.use(cors)
 app.use(express.json())
-app.listen(port)
+app.use(cors())
+
 require('dotenv').config()
 
 
+app.get("/", (req, res) => {
+    res.send("server running (. _ .)");
+});
+app.listen(port, () => {
+});
 
 const { MongoClient, ServerApiVersion, Collection } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gslw7jh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
@@ -24,8 +29,42 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
 
+        await client.connect();
         const collection = client.db("Move-Swift").collection("parcel")
 
+
+        app.post("/parcel", async (req, res) => {
+            try {
+                const parcel = req.body;
+
+                if (!parcel?.title || !parcel?.sender_name) {
+                    return res.status(400).json({ message: "Missing required fields" });
+                }
+
+                const result = await collection.insertOne(parcel);
+                res.status(201).json(result); 
+            } catch (error) {
+                console.error("Error inserting parcel:", error);
+                res.status(500).json({ message: "Internal Server Error" });
+            }
+        });
+
+        app.get('/parcels', async (req, res) => {
+            try {
+                const userEmail = req.query.email;
+
+                const query = userEmail ? { created_by: userEmail } : {};
+                const options = {
+                    sort: { createdAt: -1 }, // Newest first
+                };
+
+                const parcels = await collection.find(query, options).toArray();
+                res.send(parcels);
+            } catch (error) {
+                console.error('Error fetching parcels:', error);
+                res.status(500).send({ message: 'Failed to get parcels' });
+            }
+        });
 
         app.get("/parcel", async (req, res) => {
             const result = await collection.find().toArray();
@@ -33,13 +72,12 @@ async function run() {
         });
 
 
-        await client.connect();
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
